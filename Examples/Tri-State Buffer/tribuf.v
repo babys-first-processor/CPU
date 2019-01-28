0a4bb601fd54f44e47a7000adffd2014f35870fc
vanwarner <vanwarner@ieee.org>
2019-01-28 12:26:26 -0500

    Refresh repo with regard to .gitignore

--- Examples/Tri-State Buffer/tribuf.v
+++
@@ -1,6 +0,0 @@
-module tribuf (out, in, control);
-	input in, control;
-	output out;
-	
-	assign #2 out = control ? in : 1'bz;
-endmodule
--- simulation/modelsim/rtl_work/@c@l@a_adder64bit_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity CLA_adder64bit_tb is
-end CLA_adder64bit_tb;
